638b88c676c3840250b49c8d217944b00288e547
KhunKimMinji <141324062+KhunKimMinji@users.noreply.github.com>
2024-11-15 16:02:11 +0700

    fix fix

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -34,6 +34,9 @@ test.only("parametrized methods", async ({ page }) => {
       process.env.PASSWORD,
       "Option 1"
     );
+  await page.screenshot({ path: "screenshots/formsLayoutsPage.png" });
+  const buffer = await page.screenshot();
+  // console.log(buffer.toString("base64"));
   await pm
     .onFormLayoutsPage()
     .submitInlineFormWithNameEmailAndCheckbox(
@@ -41,6 +44,9 @@ test.only("parametrized methods", async ({ page }) => {
       randomEmail,
       true
     );
+  await page
+    .locator("nb-card", { hasText: "Inline form" })
+    .screenshot({ path: "screenshots/inkineForm.png" });
   await pm.navigateTo().datepickerPage();
   await pm.onDatepickerPage().selectCommonDatePickerFromToday(1);
   await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(5, 35);
