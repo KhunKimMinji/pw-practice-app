0568aa196ea53fcb7c6b74c185f7878c005fcdd6
KhunKimMinji <141324062+KhunKimMinji@users.noreply.github.com>
2024-11-15 15:51:50 +0700

    delete screenshort

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -34,9 +34,6 @@ test.only("parametrized methods", async ({ page }) => {
       process.env.PASSWORD,
       "Option 1"
     );
-  await page.screenshot({ path: "screenshots/formsLayoutsPage.png" });
-  const buffer = await page.screenshot();
-  console.log(buffer.toString("base64"));
   await pm
     .onFormLayoutsPage()
     .submitInlineFormWithNameEmailAndCheckbox(
@@ -44,9 +41,6 @@ test.only("parametrized methods", async ({ page }) => {
       randomEmail,
       true
     );
-  await page
-    .locator("nb-card", { hasText: "Inline form" })
-    .screenshot({ path: "screenshots/inkineForm.png" });
   await pm.navigateTo().datepickerPage();
   await pm.onDatepickerPage().selectCommonDatePickerFromToday(1);
   await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(5, 35);
